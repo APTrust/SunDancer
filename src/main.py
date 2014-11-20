5ee9554bb91e0ce1ed2bc160baecbef8e3018dc6
Streamweaver <Streamweaver@gmail.com>
2014-11-20 16:43:50 -0500

    Added method to generate fixity hash

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from sundancer.conf.settings import API_KEY, API_URL
 from sundancer.client import APIClient
-from sundancer.staging import select_nodes
+from sundancer.staging import select_nodes, list_bagnames, make_bag_symlink
+from sundancer.staging import get_sha256
 
 if __name__ == "__main__":
     aptrust = APIClient(API_URL, API_KEY)
     data = aptrust.get_nodes(filters={'replicate_to': True})
-    print(select_nodes(data["results"]))
+    node_list = select_nodes(data["results"])
+    bag_list = list_bagnames()
+    for node in node_list:
+        for bag in bag_list:
+            print(get_sha256(bag))
+            make_bag_symlink(node, bag)
--- src/sundancer/staging.py
+++ src/sundancer/staging.py
@@ -1,5 +1,5 @@
-import random
-from sundancer.conf.settings import NODE_COPIES
+import random, glob, os, hashlib
+from sundancer.conf.settings import NODE_COPIES, STAGING_DIR, NODE_DIRS
 
 """
 Deals with operates related to staging content for replication into DPN.
@@ -14,3 +14,45 @@ def select_nodes(node_list, num_nodes=NODE_COPIES):
     """
     picks = random.sample(node_list, num_nodes)
     return [pick["namespace"] for pick in picks]
+
+def list_bagnames():
+    """
+    Returns a list of all the bag filenames.
+    """
+    bag_list = []
+    for file in glob.glob("%s/*.tar" % STAGING_DIR):
+        bag_list.append(os.path.basename(file))
+    return bag_list
+
+def make_bag_symlink(node, filename):
+    """
+    Creates a symlink in the outbound directory for the named node for the
+    staged bag file provided.
+
+    :param node: String namespace of node to stage for.
+    :param filename: String basename of bag file.
+    :return:
+    """
+    try:
+        src = os.path.join(STAGING_DIR, filename)
+        dest = os.path.join(NODE_DIRS[node]['CONTENT_DIR'], filename)
+        dest = os.path.normpath(dest)
+        os.symlink(src, dest)
+    except FileExistsError:
+        pass
+
+def get_sha256(bagname):
+    """
+    Returns the sha256 hex hash of a file.
+    :param bagname:  String of the path to the file
+    :return: String of the hex value of the checksum.
+    """
+    size = 65536
+    fname = os.path.join(STAGING_DIR, bagname)
+    sha2 = hashlib.sha256()
+    with open(fname, 'rb') as f:
+        buf = f.read(size)
+        while len(buf) > 0:
+            sha2.update(buf)
+            buf = f.read(size)
+    return sha2.hexdigest()
